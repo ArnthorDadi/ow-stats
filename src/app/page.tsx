98de9437cff7a66a64b284ac3a080fdf1f68df10
ArnthorDadi <arnthordj@gmail.com>
2024-09-14 00:14:40 +0000

    Fixed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
-import { LatestPost } from "./_components/post";
-import { getServerAuthSession } from "../server/auth";
-import { api, HydrateClient } from "../trpc/server";
+import { LatestPost } from "@/components/post";
+import { getServerAuthSession } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -15,7 +15,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Overwatch <span className="text-[hsl(280,100%,70%)]">Statistics</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
